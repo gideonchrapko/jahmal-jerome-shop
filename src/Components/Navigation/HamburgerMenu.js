0f98b4a0178a87d6903abe3fd609fa07efe42bcf
Gideon Chrapko <gideonchrapko@hotmail.com>
2021-12-26 23:10:28 -0800

    fixzed the navigation bar I think

--- src/Components/Navigation/HamburgerMenu.js
+++ src/Components/Navigation/HamburgerMenu.js
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useSpring, animated } from 'react-spring'
+// import { useSpring, animated } from 'react-spring'
 import Close from "../../Assets/Close.svg";
 import Open from "../../Assets/Open.svg";
 import './hamburgerMenu.css';
 
   export default function MenuRight() {
     const [rightMenuVisible, setRightMenuVisible] = useState(false);
-    const rightMenuAnimation = useSpring({
-      opacity: rightMenuVisible ? 1 : 0,
-      transform: rightMenuVisible ? `translateX(0)` : `translateX(200%)`
-    }); 
+    // const rightMenuAnimation = useSpring({
+    //   opacity: rightMenuVisible ? 1 : 0,
+    //   transform: rightMenuVisible ? `translateX(0)` : `translateX(200%)`
+    // }); 
+
+    console.log(rightMenuVisible)
 
     return (
       <div style={{ position: "fixed", zIndex: "9999" }}>
@@ -20,7 +22,16 @@ import './hamburgerMenu.css';
 					onClick={() => setRightMenuVisible(!rightMenuVisible)}
 					src={rightMenuVisible ? Close : Open}
         />
-        <animated.div style={rightMenuAnimation} className="menu menu--right">
+        <div 
+          style={{
+            transform: `${rightMenuVisible ? "translateX(0)" : "translateX(100%)" }`,
+            opacity: `${rightMenuVisible ? "1" : "0" }`,
+            // opacity: "1",
+            // transformX: "translateX(100%)",
+            transition: "transform 0.5s"
+          }}
+          className="menu menu--right"
+        >
           <nav style={{ paddingTop: "20px", paddingBottom: "20px" }}> 
                 <li className="menu-list-item menu-list-item--right">
                   <Link to="/">
@@ -43,7 +54,7 @@ import './hamburgerMenu.css';
                   </Link>
                 </li>
           </nav> 
-      </animated.div >
+      </div >
     </div>
     );
 }
